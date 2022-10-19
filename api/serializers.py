edf9d0a398c7afa04688e7122844eaa9d76ee32e
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2022-10-19 19:31:00 +0200

    serializers2

---
+++ api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from models import Person, Genre, Movie
+
+class MovieSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['title', 'director', 'screenplay', 'starring', 'year', 'rating', 'genre']
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['first_name', 'last_name']
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['name']
--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from models import Movie, Person, Genre
+from serializers import MovieSerializer, PersonSerializer, GenreSerializer
 
 # Create your views here.
+class MovieView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
