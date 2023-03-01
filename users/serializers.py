c6af6caae10167fd09472e77238ef8968fe74dda
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2023-03-01 23:39:31 +0100

    adding RegisterSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,4 +5,15 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data)
+        return user
